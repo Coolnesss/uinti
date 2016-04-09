c4177599dc4b9c2d71e691f482156c24110170a0
Chang <chang.rajani@gmail.com>
2016-04-09 18:29:05 +0300

    Set cache to memory

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module Uinti
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << Rails.root.join('lib')
+    config.cache_store = :memory_store
   end
 end
