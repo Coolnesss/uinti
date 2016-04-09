a182f007f08c90c558baba56ab64cb9573de746d
Chang <chang.rajani@gmail.com>
2016-04-09 17:58:45 +0300

    remove rack cache

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'rest-client'
 require 'restclient/components'
-require 'rack/cache'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
