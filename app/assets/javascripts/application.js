1cc555ba6f276051411f98050ba087d77d3792d4
Chang <chang.rajani@gmail.com>
2016-04-09 18:08:24 +0300

    precompile assets, change js include order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require_tree .
-//= require material
 //= require moment
 //= require bootstrap-datetimepicker
+//= require material
+//= require_tree .
